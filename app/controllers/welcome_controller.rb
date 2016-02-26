ba429c9b86478408cf1afec1a556d6ac4144257f
Justin Thomas <jthomas0209@gmail.com>
2016-02-26 15:47:49 -0500

    Fixed routes and modified welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def home
+    redirect_to user_path(current_user) if logged_in?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   end  
   
   get '/', to: 'welcome#home', as: :home
-  
+  root 'welcome#home'
 end
