d94062b89864e619b3760537d5098c0901843256
Justin Thomas <jthomas0209@gmail.com>
2016-02-29 10:08:41 -0500

    Fixed user logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
   
   def show
-    @user = User.find(params[:id])
+    if User.exists?(params[:id])
+      @user = User.find(params[:id])
+    else
+      flash[:alet] = "User does not exist"
+      home_redirect
+    end
   end
 
 end
